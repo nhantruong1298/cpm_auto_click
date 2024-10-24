34334c1a05bfa7f071fd1d57101d075701c01983
Nhan Truong <nhantruong1298@gmail.com>
2024-10-24 12:01:01 +0700

    configure build for windows

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(620, 570);
   if (!window.Create(L"cpm_auto_click", origin, size)) {
     return EXIT_FAILURE;
   }
